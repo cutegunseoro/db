91065f2c898d2ccdfa069f207b4e09d0dbcb7b3e
Gwon Hong <gwonhong.dev@gmail.com>
2024-11-24 14:41:44 +0900

    update travel table

--- migrations/002_create_travel_table.sql
+++ migrations/002_create_travel_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE travel (
     travel_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT UNSIGNED,
+    title VARCHAR(255),
     start_datetime DATETIME NOT NULL,
     end_datetime DATETIME NOT NULL,
-    description VARCHAR(255),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES member(member_id) ON DELETE SET NULL,
     CONSTRAINT chk_datetime_range CHECK (end_datetime >= start_datetime),
